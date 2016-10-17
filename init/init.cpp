fd00df4ee613aab16f1e9c5443d21f192d768ca7
Gary Bisson <gary.bisson@boundarydevices.com>
2016-10-17 15:53:22 +0200

    init: allow to override ro.sf.hwrotation from bootargs
    When a 'androidboot.hwrotation' bootargs is provided, the
    'ro.sf.hwrotation' will be set to that value, overriding any value
    contained in the init.rc since ro.* variables can only be set once.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- init/init.cpp
+++ init/init.cpp
@@ -496,6 +496,7 @@ static void export_kernel_boot_props() {
         { "ro.boot.hardware",   "ro.hardware",   "unknown", },
         { "ro.boot.revision",   "ro.revision",   "0", },
         { "ro.boot.lcd_density", "ro.sf.lcd_density", NULL, },
+        { "ro.boot.hwrotation",  "ro.sf.hwrotation",  NULL, },
     };
     for (size_t i = 0; i < arraysize(prop_map); i++) {
         std::string value = GetProperty(prop_map[i].src_prop, "");
